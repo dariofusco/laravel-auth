01b36eebd7cb1ab59215934442c1713197d8c15f
dariofusco <fdario@hotmail.it>
2023-10-06 18:01:01 +0200

    aggiunge validazione form

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -10,32 +10,50 @@
 
             <div class="mb-3">
                 <label class="form-label">Nome</label>
-                <input type="text" class="form-control" name="name">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" name="name">
+                @error('name')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Tipologia</label>
-                <input type="text" class="form-control" name="type">
+                <input type="text" class="form-control @error('type') is-invalid @enderror" name="type">
+                @error('type')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Descrizione</label>
-                <textarea class="form-control" name="description"></textarea>
+                <textarea class="form-control @error('description') is-invalid @enderror" name="description"></textarea>
+                @error('description')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Data</label>
-                <input type="date" class="form-control" name="date">
+                <input type="date" class="form-control @error('date') is-invalid @enderror" name="date">
+                @error('date')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">Immagine</label>
-                <input type="text" class="form-control" name="image">
+                <input type="text" class="form-control @error('image') is-invalid @enderror" name="image">
+                @error('image')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="mb-3">
                 <label class="form-label">GitHub Link</label>
-                <input type="text" class="form-control" name="github_link">
+                <input type="text" class="form-control @error('github_link') is-invalid @enderror" name="github_link">
+                @error('github_link')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <button class="btn btn-primary">Crea</button>
